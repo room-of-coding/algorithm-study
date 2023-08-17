557f6e28aca777c14357ecfd250ad8c77cc85cbd
janjanee92 <watrv41@gmail.com>
2023-08-17 11:34:43 +0900

    chore:주석

--- programmers/분수의_덧셈/jhlee.py
+++ programmers/분수의_덧셈/jhlee.py
@@ -1,10 +1,16 @@
 from fractions import Fraction
 
 def solution(numer1, denom1, numer2, denom2):
+    """
+    python 분수 Fraction 클래스를 사용
+    """
     result = Fraction(numer1, denom1) + Fraction(numer2, denom2)
     return [result.numerator, result.denominator]
 
 def solution_2(numer1, denom1, numer2, denom2):
+    """
+    유클리드 호제법 재귀를 사용
+    """
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
     gsd = getGsd(denom, numer)
