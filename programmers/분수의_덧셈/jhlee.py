59c2f071f9689a6945777baf49e839be34d93f7f
janjanee92 <watrv41@gmail.com>
2023-08-17 13:26:40 +0900

    gsd 늪에 빠져버린 gcd 오타수정

--- programmers/분수의_덧셈/jhlee.py
+++ programmers/분수의_덧셈/jhlee.py
@@ -13,8 +13,8 @@ def solution_2(numer1, denom1, numer2, denom2):
     """
     numer = numer1 * denom2 + numer2 * denom1
     denom = denom1 * denom2
-    gsd = getGsd(denom, numer)
+    gcd = getGcd(denom, numer)
     return [numer / gsd, denom / gsd]
 
-def getGsd(a, b):
-    return getGsd(b, a % b) if a % b != 0 else b
+def getGcd(a, b):
+    return getGcd(b, a % b) if a % b != 0 else b
